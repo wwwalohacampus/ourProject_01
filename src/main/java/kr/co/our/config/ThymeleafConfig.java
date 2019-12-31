8fa3f537b126a662440f3062fd75b2c320ad8f4d
ALOHA <ALOHA@DESKTOP-FBBK53F>
2019-12-31 15:08:27 +0900

    aaaa

---
+++ src/main/java/kr/co/our/config/ThymeleafConfig.java
@@ -0,0 +1,27 @@
+package kr.co.our.config;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+
+    ...
+
+    @Bean
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.addDialect(new LayoutDialect());
+        return templateEngine;
+    }
+
+    ...
+}
