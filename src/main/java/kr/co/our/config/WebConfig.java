32fed2c61e36ac55968e27b3a727dbf1b5cd3247
ALOHA <ALOHA@DESKTOP-FBBK53F>
2019-12-31 22:53:46 +0900

    dkdjdk

--- src/main/java/kr/co/our/config/WebConfig.java
+++ src/main/java/kr/co/our/config/WebConfig.java
@@ -1,6 +1,7 @@
 package kr.co.our.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
 import org.springframework.web.servlet.ViewResolver;
@@ -8,25 +9,29 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 import lombok.var;
 
 
 @Configuration
+@ComponentScan(basePackages = "templates")
 public class WebConfig implements WebMvcConfigurer {
 
 	@Bean
 	@Description("Thymeleaf template resolver serving HTML 5")
 	public ClassLoaderTemplateResolver templateResolver() {
 		
-		var templateResolver = new ClassLoaderTemplateResolver();
+		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
 		
-		templateResolver.setPrefix("/templates/");
-		templateResolver.setCacheable(false);
+		templateResolver.setPrefix("templates/");
 		templateResolver.setSuffix(".html");
-		templateResolver.setTemplateMode("HTML5");
+		templateResolver.setTemplateMode(TemplateMode.HTML);
 		templateResolver.setCharacterEncoding("UTF-8");
+		templateResolver.setOrder(0);
+		templateResolver.setCacheable(false);
+		templateResolver.setCheckExistence(true);
 		
 		return templateResolver;
 	}
