04d248ba8491140213d82113bca408888c0ca5ff
ALOHA <ALOHA@DESKTOP-FBBK53F>
2020-01-02 16:03:19 +0900

    asdf

--- src/main/java/kr/co/our/config/WebConfig.java
+++ src/main/java/kr/co/our/config/WebConfig.java
@@ -39,8 +39,7 @@ public class WebConfig implements WebMvcConfigurer {
 		
 		templateResolver.setPrefix("templates/");
 		templateResolver.setSuffix(".html");
-//		templateResolver.setTemplateMode(TemplateMode.HTML);
-		templateResolver.setTemplateMode("HTML5");
+		templateResolver.setTemplateMode(TemplateMode.HTML5);
 		templateResolver.setCharacterEncoding("UTF-8");
 		templateResolver.setOrder(0);
 		templateResolver.setCacheable(false);
@@ -57,7 +56,7 @@ public class WebConfig implements WebMvcConfigurer {
 		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
 		templateEngine.setTemplateResolver(templateResolver());
 		templateEngine.setEnableSpringELCompiler(true);
-   	 	templateEngine.addDialect(new LayoutDialect());			// **
+//   	 	templateEngine.addDialect(new LayoutDialect());			// **
 		
 		return templateEngine;
 	}
