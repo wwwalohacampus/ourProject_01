7c5251b55018a24cb966e0e1d9841387d2a87d95
ALOHA <ALOHA@DESKTOP-FBBK53F>
2020-01-02 17:41:20 +0900

    asdfasdf

--- src/main/java/kr/co/our/config/WebConfig.java
+++ src/main/java/kr/co/our/config/WebConfig.java
@@ -1,74 +1,54 @@
 package kr.co.our.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Description;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-
-import nz.net.ultraq.thymeleaf.LayoutDialect;
-
-
 
 @Configuration
+@EnableWebMvc
+@ComponentScan("kr.co.our.controller")
 @ComponentScan(basePackages = "templates")
 public class WebConfig implements WebMvcConfigurer {
 	
-//	@Override
-//	public void addViewControllers(ViewControllerRegistry registry) {
-//		registry.addViewController("/").setViewName("index");
-//	}
-
+	@Autowired
+	private ApplicationContext applicationContext;
+	
 	@Bean
-	@Description("Thymeleaf template resolver serving HTML 5")
-	public ClassLoaderTemplateResolver templateResolver() {
-		
-		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-		
-		templateResolver.setPrefix("templates/");
-		templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode(TemplateMode.HTML);
-		templateResolver.setCharacterEncoding("UTF-8");
-		templateResolver.setOrder(0);
-		templateResolver.setCacheable(false);
-//		templateResolver.setCheckExistence(true);
+	public SpringResourceTemplateResolver templateResolver() {
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+	      templateResolver.setApplicationContext(applicationContext);
+	      templateResolver.setPrefix("templates/");
+	      templateResolver.setSuffix(".html");
 		
 		return templateResolver;
 	}
 	
 	
 	@Bean
-	@Description("Thymelaef template engine with Spring integration")
-	public SpringTemplateEngine templateEngine() {
-		
-		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-		templateEngine.setTemplateResolver(templateResolver());
-//		templateEngine.setEnableSpringELCompiler(true);
-//   	templateEngine.addDialect(new LayoutDialect());			// **
-		templateEngine.addDialect(new SpringSecurityDialect());
-		
-		
-		return templateEngine;
+   public SpringTemplateEngine templateEngine() {
+      SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+      templateEngine.setTemplateResolver(templateResolver());
+      templateEngine.setEnableSpringELCompiler(true);
+      return templateEngine;
+   }
+
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		registry.viewResolver(resolver);
 	}
 	
-	@Bean
-	@Description("Thymeleaf view resolver")
-	public ViewResolver viewResolver() {
-		
-		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-		viewResolver.setTemplateEngine(templateEngine());
-		viewResolver.setCharacterEncoding("UTF-8");
-		
-		return viewResolver;
-	}
-
+	
 	
 	
 }
