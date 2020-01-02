1c7a528941e2644d81b0cff09f2fca999c8d2058
ALOHA <ALOHA@DESKTOP-FBBK53F>
2020-01-02 14:11:21 +0900

    asdfasfads

--- src/main/java/kr/co/our/config/WebConfig.java
+++ src/main/java/kr/co/our/config/WebConfig.java
@@ -70,7 +70,7 @@ public class WebConfig implements WebMvcConfigurer {
 		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
 		viewResolver.setTemplateEngine(templateEngine());
 		viewResolver.setCharacterEncoding("UTF-8");
-		viewResolver.setOrder(1);
+		viewResolver.setOrder(0);
 		
 		return viewResolver;
 	}
