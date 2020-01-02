50b905066ece7f00892aa4ddd86656fbba387699
ALOHA <ALOHA@DESKTOP-FBBK53F>
2020-01-02 17:52:47 +0900

    asdfasdf

--- src/main/java/kr/co/our/config/WebConfig.java
+++ src/main/java/kr/co/our/config/WebConfig.java
@@ -6,11 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
@@ -37,34 +33,6 @@ public class WebConfig implements WebMvcConfigurer {
 		
 		return templateResolver;
 	}
-	
-	@Bean
-	public SpringResourceTemplateResolver templateResolver() {
-		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-	      templateResolver.setApplicationContext(applicationContext);
-	      templateResolver.setPrefix("templates/");
-	      templateResolver.setSuffix(".html");
-		
-		return templateResolver;
-	}
-	
-	
-	@Bean
-   public SpringTemplateEngine templateEngine() {
-      SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-      templateEngine.setTemplateResolver(classLoaderTemplateResolver());
-      templateEngine.setEnableSpringELCompiler(true);
-      return templateEngine;
-   }
-
-
-	@Override
-	public void configureViewResolvers(ViewResolverRegistry registry) {
-		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-		resolver.setTemplateEngine(templateEngine());
-		registry.viewResolver(resolver);
-	}
-	
 	
 	
 	
