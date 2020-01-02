b6448f7ad05c97be421967de2d6a15bc7f86da3f
ALOHA <ALOHA@DESKTOP-FBBK53F>
2020-01-02 13:38:18 +0900

    aaaa

--- src/main/java/kr/co/our/config/WebConfig.java
+++ src/main/java/kr/co/our/config/WebConfig.java
@@ -36,7 +36,7 @@ public class WebConfig implements WebMvcConfigurer {
 		return templateResolver;
 	}
 	
-	/*
+	
 	@Bean
 	@Description("Thymelaef template engine with Spring integration")
 	public SpringTemplateEngine templateEngine() {
@@ -58,16 +58,6 @@ public class WebConfig implements WebMvcConfigurer {
 		
 		return viewResolver;
 	}
-
-
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("index");
-	}
-	
-	*/
-	
-	
 	
 	
 }
