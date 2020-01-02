89dfb29ca86e3080505a00143475cfc7bbefd933
ALOHA <ALOHA@DESKTOP-FBBK53F>
2020-01-02 15:50:02 +0900

    asdfads

--- src/main/java/kr/co/our/config/WebConfig.java
+++ src/main/java/kr/co/our/config/WebConfig.java
@@ -39,7 +39,8 @@ public class WebConfig implements WebMvcConfigurer {
 		
 		templateResolver.setPrefix("templates/");
 		templateResolver.setSuffix(".html");
-		templateResolver.setTemplateMode(TemplateMode.HTML);
+//		templateResolver.setTemplateMode(TemplateMode.HTML);
+		templateResolver.setTemplateMode("HTML5");
 		templateResolver.setCharacterEncoding("UTF-8");
 		templateResolver.setOrder(0);
 		templateResolver.setCacheable(false);
@@ -55,9 +56,8 @@ public class WebConfig implements WebMvcConfigurer {
 		
 		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
 		templateEngine.setTemplateResolver(templateResolver());
-//		templateEngine.setEnableSpringELCompiler(true);
-
-		// templateEngine.addDialect(new LayoutDialect());			// **
+		templateEngine.setEnableSpringELCompiler(true);
+   	 	templateEngine.addDialect(new LayoutDialect());			// **
 		
 		return templateEngine;
 	}
