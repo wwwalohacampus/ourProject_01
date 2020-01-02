690df073b64f9f2ba2302845f2b05061cfc53c77
ALOHA <ALOHA@DESKTOP-FBBK53F>
2020-01-02 16:44:09 +0900

    asdfasdf

---
+++ src/main/java/kr/co/our/config/WebConfig.java
@@ -0,0 +1,77 @@
+package kr.co.our.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+
+
+@Configuration
+@ComponentScan(basePackages = "templates")
+public class WebConfig implements WebMvcConfigurer {
+	
+//	@Override
+//	public void addViewControllers(ViewControllerRegistry registry) {
+//		registry.addViewController("/").setViewName("index");
+//	}
+
+	@Bean
+	@Description("Thymeleaf template resolver serving HTML 5")
+	public ClassLoaderTemplateResolver templateResolver() {
+		
+		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+		
+		templateResolver.setPrefix("templates/");
+		templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+		templateResolver.setCharacterEncoding("UTF-8");
+		templateResolver.setOrder(0);
+		templateResolver.setCacheable(false);
+//		templateResolver.setCheckExistence(true);
+		
+		return templateResolver;
+	}
+	
+	
+	@Bean
+	@Description("Thymelaef template engine with Spring integration")
+	public SpringTemplateEngine templateEngine() {
+		
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+//		templateEngine.setEnableSpringELCompiler(true);
+//   	 	templateEngine.addDialect(new LayoutDialect());			// **
+		
+		return templateEngine;
+	}
+	
+	@Bean
+	@Description("Thymeleaf view resolver")
+	public ViewResolver viewResolver() {
+		
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine());
+		viewResolver.setCharacterEncoding("UTF-8");
+		
+		return viewResolver;
+	}
+
+	
+	
+}
+
+
+
+
+
+
+
+
